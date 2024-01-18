a41a70f6a12e68e6c469c8882e7b67ba7dd0cddf
Kale13-R <Kalebriley913@yahoo.com>
2024-01-18 11:20:52 -0600

    Initial Commit

---
+++ Xula shuttle tracker/Xula_shuttle_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xula_shuttle_trackerApp.swift
+//  Xula shuttle tracker
+//
+//  Created by Kaleb Riley on 1/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Xula_shuttle_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
